04bda6a3df33796045e8b92fa56fe588cf0345af
mauromendozacp <mauro.mendozacp@gmail.com>
2021-09-23 12:02:57 -0300

    entity2d added

---
+++ engine-pg1/src/Entity2D/entity2D.cpp
@@ -0,0 +1,12 @@
+#include "entity2D.h"
+
+namespace GL
+{
+	Entity2D::Entity2D(Render* render) : Entity(render)
+	{
+	}
+
+	Entity2D::~Entity2D()
+	{
+	}
+}
---
+++ engine-pg1/src/Entity2D/entity2D.h
@@ -0,0 +1,19 @@
+#ifndef ENTITY2D_H
+#define ENTITY2D_H
+
+#include "Entity/entity.h"
+
+namespace GL
+{
+	class GRAPHICSENGINE_API Entity2D : Entity
+	{
+	public:
+		Entity2D(Render* render);
+		~Entity2D();
+
+	private:
+
+	};
+}
+
+#endif // !ENTITY2D_H
